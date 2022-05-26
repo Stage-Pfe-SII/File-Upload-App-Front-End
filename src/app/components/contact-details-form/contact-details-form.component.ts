4f648998a92d526d7053d3c558b0e5f193152977
yasser nadir <yasser.nadir@sii-maroc.com>
2022-05-26 10:25:16 +0100

    add regular expression pour email

--- src/app/components/contact-details-form/contact-details-form.component.ts
+++ src/app/components/contact-details-form/contact-details-form.component.ts
@@ -24,17 +24,19 @@ export class ContactDetailsFormComponent implements OnInit {
   @Input() myStepper!: MatStepper;
   @Output() formSubmit = new EventEmitter();
   @Output() state = new EventEmitter();
+  regexMail = new RegExp("^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])$")
 
 
   matcher = new ErrorStateMatcher();
 
   constructor(private fomrBuilder:FormBuilder) {
     this.contactDetailsForm = this.fomrBuilder.group({
-      sender: ['',[Validators.required,Validators.email]],
+      sender: ['',[Validators.required,Validators.email, Validators.pattern(this.regexMail)]],
       receiver: ['',[Validators.required,Validators.email]],
       title: ['',[Validators.required]],
       message: ['']
     })
+    
    }
 
   ngOnInit(): void {
@@ -52,6 +54,10 @@ export class ContactDetailsFormComponent implements OnInit {
       this.formSubmit.emit(this.contactDetailsForm.value);
       this.myStepper.next();
     }
+  }
+
+  onChange(){
+    console.log(this.contactDetailsForm.get('sender')?.hasError('pattern'))
   }
 
 }
